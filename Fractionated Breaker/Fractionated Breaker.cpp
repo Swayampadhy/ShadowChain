1d4ca11c1a5eb8b17edb9fe4ee6e18f08db1d47d
Swayam <swayamtejasp@gmail.com>
2024-12-29 10:47:36 -0800

    added strlen and create fraction function

--- Fractionated Breaker/Fractionated Breaker.cpp
+++ Fractionated Breaker/Fractionated Breaker.cpp
@@ -20,6 +20,55 @@ BOOL IsPathValidW(PWCHAR FilePath) {
 	return TRUE;
 }
 
+SIZE_T StringLengthA(LPCSTR String) { LPCSTR String2; for (String2 = String; *String2; ++String2); return (String2 - String); }
+
+BOOL CreateFraction(PBYTE DataBlock, DWORD dwWriteSize, PWCHAR OutputDirectory) {
+	HANDLE hHandle = INVALID_HANDLE_VALUE;
+	WCHAR OutputPath[MAX_PATH * sizeof(WCHAR)] = { 0 };
+	DWORD dwOut = ERROR_SUCCESS;
+	BOOL bFlag = FALSE;
+	CHAR FileHeader[MAX_PATH] = { 0 };
+
+	for(DWORD dwFractionCount = 0;;dwFractionCount++){
+		_snwprintf_s(OutputPath, MAX_PATH * sizeof(WCHAR), L"%wsFraction%ld", OutputDirectory, dwFractionCount);
+		if (IsPathValidW(OutputPath)) {
+			continue;
+		}
+		else{
+			_snprintf_s(FileHeader, MAX_PATH, "<%ld>", dwFractionCount);
+
+			if (strlen(FileHeader) < 32) {
+				DWORD dwOffset = (DWORD)(32 - strlen(FileHeader));
+				for (DWORD dwX = 0; dwX < dwOffset; dwX++) {
+					strcat(FileHeader, " ");
+				}
+				break;
+			}
+		}
+
+		hHandle = CreateFileW(OutputPath, GENERIC_WRITE, 0, NULL, CREATE_NEW, FILE_ATTRIBUTE_NORMAL, NULL);
+		if (hHandle == INVALID_HANDLE_VALUE) {
+			goto EXIT_ROUTINE;
+		}
+
+		if (!WriteFile(hHandle, FileHeader, 32, &dwOut, NULL)) {
+			goto EXIT_ROUTINE;
+		}
+		dwOut = ERROR_SUCCESS;
+
+		if (!WriteFile(hHandle, DataBlock, dwWriteSize, &dwOut, NULL)) {
+			goto EXIT_ROUTINE;
+		}
+
+	EXIT_ROUTINE:
+
+		if (hHandle)
+			CloseHandle(hHandle);
+
+		return bFlag;
+
+}
+
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
 	HANDLE hHandle = INVALID_HANDLE_VALUE;
