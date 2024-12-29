8b0c16d6c572fb266ba706e3cd9817d14961a60f
Swayam <swayamtejasp@gmail.com>
2024-12-29 09:34:44 -0800

    added struct and filepath checking

--- Fractionated Breaker/Fractionated Breaker.cpp
+++ Fractionated Breaker/Fractionated Breaker.cpp
@@ -1,6 +1,25 @@
 #include <Windows.h>
 #include <stdio.h>
 
+typedef struct __FRACTION_DATA {
+	LONGLONG BufferSize;
+	DWORD NumberOfFractions;
+} FRACTION_DATA, * PFRACTION_DATA;
+
+PBYTE g_BinaryBuffer = NULL;
+
+BOOL IsPathValidW(PWCHAR FilePath) {
+	HANDLE hFile = INVALID_HANDLE_VALUE;
+	hFile = CreateFile(FilePath, GENERIC_READ, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+	if (hFile == INVALID_HANDLE_VALUE) {
+		return FALSE;
+	}
+
+	if (hFile)
+		CloseHandle(hFile);
+	return TRUE;
+}
+
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
 	HANDLE hHandle = INVALID_HANDLE_VALUE;
