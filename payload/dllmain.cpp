d26e657fc82d2ee984ae8b7ce80f8c4fcf83fe5b
Swayam <swayamtejasp@gmail.com>
2024-12-30 03:21:31 -0800

    created payload

--- payload/dllmain.cpp
+++ payload/dllmain.cpp
@@ -1,18 +1,29 @@
 #include "pch.h"
 
+// Function to store the payload
+VOID Payload() {
+	MessageBoxA(NULL, "Payload", "Payload", MB_OK | MB_ICONINFORMATION);
+}
+
 BOOL APIENTRY DllMain( HMODULE hModule,
-                       DWORD  ul_reason_for_call,
+                       DWORD  dwReason,
                        LPVOID lpReserved
                      )
 {
-    switch (ul_reason_for_call)
+    switch (dwReason)
     {
-    case DLL_PROCESS_ATTACH:
+    // Runs the payload when a process is attached to it
+    case DLL_PROCESS_ATTACH: {
+		Payload();
+		break;
+    }
     case DLL_THREAD_ATTACH:
     case DLL_THREAD_DETACH:
+    // Breaks when detached
     case DLL_PROCESS_DETACH:
         break;
     }
+
     return TRUE;
 }
 
