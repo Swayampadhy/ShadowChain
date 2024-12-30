76121d382ec0479da56daffc365ba6d07a4834df
Swayam <swayamtejasp@gmail.com>
2024-12-30 03:34:08 -0800

    finished starter code

--- ShadowChain/ShadowChain.c
+++ ShadowChain/ShadowChain.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
+#include <Windows.h>
 
-int main() {
-	//printf("Hello, World!\n");
+int main(int argc, char* argv[]) {
+	
+	if (argc < 2) {
+		printf("[!] Missing Argument; Dll payload to run\n");
+		return -1;
+	}
+
+	printf("[*] Running Dll Payload: %s\n", argv[1]);
+	printf("[i] Injecting \"%s\" To The Local Process Of Pid: %d\n", argv[1], GetCurrentProcessId());
+
+	printf("[+] Loading Dll ...");
+	if (LoadLibraryA(argv[1]) == NULL) {
+		printf("[!] Loadlibrary Failed With Error Code: %d\n", GetLastError());
+		return -1;
+	}
+
+	printf("[+] Dll Loaded Successfully\n");
+
+	printf("[#] Press Any Key To Exit\n");
+	getchar();
+
 	return 0;
 }	
