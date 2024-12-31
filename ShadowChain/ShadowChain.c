5208dd65b68ba503e60a8330e1bdf34aa4aea884
Swayam <swayamtejasp@gmail.com>
2024-12-31 03:00:48 -0800

    will work with shellcode later

--- ShadowChain/ShadowChain.c
+++ ShadowChain/ShadowChain.c
@@ -8,14 +8,14 @@ int main(int argc, char* argv[]) {
 		return -1;
 	}
 
-	//printf("[*] Running Dll Payload: %s\n", argv[1]);
-	//printf("[i] Injecting \"%s\" To The Local Process Of Pid: %d\n", argv[1], GetCurrentProcessId());
+	printf("[*] Running Dll Payload: %s\n", argv[1]);
+	printf("[i] Injecting \"%s\" To The Local Process Of Pid: %d\n", argv[1], GetCurrentProcessId());
 
-	//printf("[+] Loading Dll ...");
-	//if (LoadLibraryA(argv[1]) == NULL) {
-	//	printf("[!] Loadlibrary Failed With Error Code: %d\n", GetLastError());
-	//	return -1;
-	//}
+	printf("[+] Loading Dll ...");
+	if (LoadLibraryA(argv[1]) == NULL) {
+		printf("[!] Loadlibrary Failed With Error Code: %d\n", GetLastError());
+		return -1;
+	}
 
 	printf("[+] Payload Loaded Successfully\n");
 
